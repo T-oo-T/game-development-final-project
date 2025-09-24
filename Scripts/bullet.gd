b56c617fa7c0ee8ca45b371ee803ae105b96255f
Otto Peräkylä <otto.perakyla@gmail.com>
2025-09-24 18:11:27 +0300

    bullet lifetime

--- Scripts/bullet.gd
+++ Scripts/bullet.gd
@@ -1,11 +1,21 @@
 class_name Bullet extends Area2D
 
-const SPEED = 30.0
+const SPEED = 60.0
 
 var facing_right
+var lifetime
 
 func _ready() -> void:
+	lifetime = Timer.new()
+	add_child(lifetime)
+	lifetime.wait_time = 1.0
+	lifetime.one_shot = true
+	lifetime.start()
+	lifetime.timeout.connect(_on_lifetime_end)
 	$AnimatedSprite2D.play("shoot")
+	
+func _on_lifetime_end() -> void:
+	queue_free()
 	
 func _process(delta: float) -> void:
 	var direction = 1.0 if facing_right else -1.0
