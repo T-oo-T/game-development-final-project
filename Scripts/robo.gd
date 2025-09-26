85606055565b0e918d42a993aed52f9e06fc4152
Otto Peräkylä <otto.perakyla@gmail.com>
2025-09-26 18:45:07 +0300

    make robo sleep if player is far

--- Scripts/robo.gd
+++ Scripts/robo.gd
@@ -5,18 +5,31 @@ var facing_left := true
 @onready var ray := $RayCast2D
 
 var GRAVITY := float(ProjectSettings.get_setting("physics/2d/default_gravity"))
+var player: CharacterBody2D
 
 func _ready() -> void:
-	$AnimatedSprite2D.play("move")
+	$AnimatedSprite2D.play("sleep")
+	player = get_parent().get_children().filter(func (child) -> bool:
+		return child is Player
+	)[0]
 	
 func _physics_process(delta: float) -> void:
+	var distance = (player.global_position - global_position).length()
+	var player_near = distance < 100
+	
 	if !is_on_floor():
 		velocity.y += GRAVITY * delta
 	
 	if !ray.is_colliding() and is_on_floor():
 		flip()
 	
-	velocity.x = speed
+	if player_near:
+		velocity.x = speed
+		$AnimatedSprite2D.play("move")
+	else:
+		velocity.x = 0
+		$AnimatedSprite2D.play("sleep")
+	
 	move_and_slide()
 		
 
--- Scripts/ui.gd
+++ Scripts/ui.gd
@@ -1,6 +1,12 @@
 class_name UI extends CanvasLayer
 
 # TODO: make UI and background scale when maximizing game
+# TODO: Gamer can see how fast she passes the level
+# TODO: There are various maps with _increasing_ difficulty
+# TODO: The enemies have “some intelligence” (state machine is enough)
+# TODO: All the settings and game records (high score / TOP10, passed levels, etc) are stored in a save file(s)
+# TODO: Shader effects are used to improve the visual style and immersion
+# TODO: Music adapts to game situations (e.g. the health level is lower, music is more intensive)
 func set_score(score: int) -> void:
 	$Score/Label.text = str(score)
 
