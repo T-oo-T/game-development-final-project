c07ae5704fd73ecd58043007ecda942628407b71
Otto Peräkylä <otto.perakyla@gmail.com>
2025-09-28 10:03:07 +0300

    flier

--- Scripts/flier.gd
+++ Scripts/flier.gd
@@ -2,28 +2,26 @@ class_name Flier extends CharacterBody2D
 
 var speed := 30
 var player: CharacterBody2D
+var state: int
+const STATE_IDLE = 0
+const STATE_CHASE = 1
 
 func _ready() -> void:
+	state = STATE_IDLE
 	player = get_parent().get_children().filter(func (child) -> bool:
 		return child is Player
 	)[0]
 	
 func _physics_process(delta: float) -> void:
 	var towardsPlayer = player.global_position - global_position
-	var distance = towardsPlayer.length()
-	var player_near = distance < 300
-	var direction = towardsPlayer.normalized()
-
-	if is_on_wall():
-		direction = Vector2(0, -1)
-		
-	if player_near:
+	
+	if state == STATE_CHASE:
+		var direction = Vector2.UP if is_on_wall() else towardsPlayer.normalized()
 		velocity.x = direction[0] * speed
 		velocity.y = direction[1] * speed
-	else:
-		velocity.x = 0
-		velocity.y = 0
-	
+	elif towardsPlayer.length() < 50:
+		state = STATE_CHASE
+
 	move_and_slide()
 		
 
